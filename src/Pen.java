4aaa10f7a5b64e467fcd4bd0dda67b9b6dbb63ab
Heikki <heikki.savolainen2@metropolia.fi>
2024-02-26 13:48:04 +0200

    task done, code working

--- src/Pen.java
+++ src/Pen.java
@@ -5,28 +5,31 @@ public class Pen {
         Color(String color) { this.color = color; };
         @Override public String toString() { return color; }
     }
+    private boolean capOn;
     private Color color;
     public Pen() {
         this.color = Color.RED;
+        this.capOn = true;
     }
     public Pen(Color color) {
         this.color = color;
+        this.capOn = true;
     }
-    public boolean capOn(){
-        return true;
+    public void capOn(){
+        capOn = true;
     }
-    public boolean capOff() {
-        return false;
+    public void capOff() {
+        capOn = false;
     }
     public String draw(){
-        if (capOn()) {
+        if (capOn) {
             return "";
         } else {
             return "Drawing " + color.toString();
         }
     }
     public void changeColor(Color color) {
-        if (capOff()) {
+        if (!capOn) {
             return;
         }
         this.color = color;
